a4545f3c5d0e423876b9d95f2ed91127434dd2f1
Aleksa Jaksic <aleksa.jaksic003@gmail.com>
2026-05-20 15:46:00 +0200

    Dodata dokumentacija za klasu User

--- src/main/java/rs/ac/bg/fon/aleksa_jaksic/sa/user/domain/User.java
+++ src/main/java/rs/ac/bg/fon/aleksa_jaksic/sa/user/domain/User.java
@@ -3,6 +3,11 @@ package rs.ac.bg.fon.aleksa_jaksic.sa.user.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+/**
+ * Represents a user within the system.
+ * Manages identity credentials and contact details.
+ * @author Aleksa Jaksic (a-jaksic)
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -12,16 +17,31 @@ import lombok.*;
 @Table(name = "users")
 public class User {
 
+    /**
+     * Unique ID.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * The unique username used by the user to log into the application.
+     */
     private String username;
 
+    /**
+     * The hashed password used to authenticate the user.
+     */
     private String password;
 
+    /**
+     * The official email address linked to the user.
+     */
     private String email;
 
+    /**
+     * The system privilege level assigned to the user.
+     */
     @Enumerated(EnumType.STRING)
     private Role role;
 }
