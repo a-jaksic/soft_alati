bf0f574d09558b7ca3fece02112a8e88344f9db8
Aleksa Jaksic <aleksa.jaksic003@gmail.com>
2026-05-20 15:38:28 +0200

    Dodata dokumentacija za klasu Review

--- src/main/java/rs/ac/bg/fon/aleksa_jaksic/sa/review/domain/Review.java
+++ src/main/java/rs/ac/bg/fon/aleksa_jaksic/sa/review/domain/Review.java
@@ -8,6 +8,11 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import java.time.LocalDateTime;
 
+/**
+ * Represents a user review submitted for a specific restaurant.
+ * Holds the user's score and text content
+ * @author Aleksa Jaksic (a-jaksic)
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -17,23 +22,44 @@ import java.time.LocalDateTime;
 @Table(name = "reviews")
 public class Review {
 
+    /**
+     * Unique ID.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * The numeric score of the review.
+     */
     private Integer rating;
 
+    /**
+     * The title of the review.
+     */
     private String title;
 
+    /**
+     * The textual content describing the user's dining experience.
+     */
     private String description;
 
+    /**
+     * The timestamp indicating when the review was created.
+     */
     private LocalDateTime createdAt;
 
+    /**
+     * The user who is the author and submitted the review.
+     */
     @ManyToOne
     @JoinColumn(name = "user_id")
     @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 
+    /**
+     * The restaurant that the review is about.
+     */
     @ManyToOne
     @JoinColumn(name = "restaurant_id")
     @OnDelete(action = OnDeleteAction.CASCADE)
