bc1c78edc5b1e7fd790d15878df5f334678f211a
Aleksa Jaksic <aleksa.jaksic003@gmail.com>
2026-05-20 15:24:55 +0200

    Dodata dokumentacija za klasu RestaurantType

--- src/main/java/rs/ac/bg/fon/aleksa_jaksic/sa/restauranttype/domain/RestaurantType.java
+++ src/main/java/rs/ac/bg/fon/aleksa_jaksic/sa/restauranttype/domain/RestaurantType.java
@@ -2,7 +2,11 @@ package rs.ac.bg.fon.aleksa_jaksic.sa.restauranttype.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
-
+/**
+ * Represents the cuisine style of a restaurant.
+ * Enforces a unique constraint on the name to prevent duplicate categories.
+ * @author Aleksa Jaksic (a-jaksic)
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -14,9 +18,14 @@ import lombok.*;
 })
 public class RestaurantType {
 
+    /**
+     * Unique ID.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    /**
+     * The name of the restaurant type category (e.g., "Italian", "Mexican").
+     */
     private String name;
 }
